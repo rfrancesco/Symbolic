26776f96dde1aebb8a419409a15a3553ea945223
Rocco Francesco Basta <rfrancesco.basta@gmail.com>
2026-06-22 03:21:55 +0200

    AST: Small comment change (on deprecated node.evaluate())

--- lib/include/Symbolic/Core/AST/Node.hpp
+++ lib/include/Symbolic/Core/AST/Node.hpp
@@ -10,7 +10,12 @@ namespace Symbolic::Core
     class Node
     {
     public:
-        // warning: evaluate() will be deprecated. Use Evaluator algorithm instead
+        // warning: evaluate() will be deprecated. Use Symbolic::Core::Evaluator algorithm instead:
+        // see Symbolic/Core/Algorithms/Evaluator.hpp or examples/01_basic_usage.cpp
+        // 
+        // Leaving this implementation here so that I can perform some more tests
+        // before removing this from the inheritance tree.
+        // No [[deprecated]] because it would create a bunch of irrelevant compiler warnings
         virtual double evaluate(const SymbolContext &) const = 0;
         // todo: printing needs to be refactored into Visitor too!
         virtual void print(std::ostream &) const = 0;
