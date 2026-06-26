534eb76ea59da6b03d3908044d6c9c735028c279
Rocco Francesco Basta <rfrancesco.basta@gmail.com>
2026-06-26 14:52:37 +0200

    Refactor: Separate NodeStorage into its own header

--- lib/include/Symbolic/Core/AST/Expression.hpp
+++ lib/include/Symbolic/Core/AST/Expression.hpp
@@ -5,51 +5,13 @@
 #include <memory>
 
 #include "Symbolic/Core/Types.hpp"
+#include "Symbolic/Core/AST/NodeStorage.hpp"
 #include "Symbolic/Core/AST/Nodes/Node.hpp"
 #include "Symbolic/Core/AST/Nodes/Symbol.hpp"
 #include "Symbolic/Core/AST/Nodes/FunctionNode.hpp"
 
 namespace Symbolic::Core
 {
-    // Should be refactored away from public API into internals?
-    class NodeStorage
-    {
-    private:
-        template <typename T>
-        const T *storeNode(std::unique_ptr<T> owner)
-        {
-            T *raw = owner.get();
-            storage.push_back(std::move(owner));
-            return raw;
-        }
-
-        std::vector<std::unique_ptr<Node>> storage;
-
-    public:
-        NodeStorage() = default;
-        NodeStorage(const NodeStorage &) = delete;
-        NodeStorage(NodeStorage &&other) : storage(std::move(other.storage)) {}
-        NodeStorage& operator=(const NodeStorage&) = delete;
-        NodeStorage& operator=(NodeStorage&&) = delete;
-
-        template <typename T, typename... Args>
-        const T *makeNode(Args &&...args)
-        {
-            return storeNode(std::make_unique<T>(std::forward<Args>(args)...));
-        }
-
-        template <typename T>
-        const T *makeNode(std::initializer_list<const Node *> children)
-        {
-            return storeNode(std::make_unique<T>(children));
-        }
-
-        const FunctionNode *makeFunctionNode(const Function &f, std::vector<const Node *> children)
-        {
-            return storeNode(std::make_unique<FunctionNode>(f, children));
-        }
-    };
-
     class Expression
     {
     private:
---
+++ lib/include/Symbolic/Core/AST/NodeStorage.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Symbolic/Core/AST/Nodes/Node.hpp"
+#include "Symbolic/Core/AST/Nodes/FunctionNode.hpp"
+#include "Symbolic/Core/Functions/Function.hpp"
+#include <memory>
+#include <vector>
+
+namespace Symbolic::Core
+{
+    class NodeStorage
+    {
+    private:
+        template <typename T>
+        const T *storeNode(std::unique_ptr<T> owner)
+        {
+            T *raw = owner.get();
+            storage.push_back(std::move(owner));
+            return raw;
+        }
+
+        std::vector<std::unique_ptr<Node>> storage;
+
+    public:
+        NodeStorage() = default;
+        NodeStorage(const NodeStorage &) = delete;
+        NodeStorage(NodeStorage &&other) : storage(std::move(other.storage)) {}
+        NodeStorage &operator=(const NodeStorage &) = delete;
+        NodeStorage &operator=(NodeStorage &&) = delete;
+
+        template <typename T, typename... Args>
+        const T *makeNode(Args &&...args)
+        {
+            return storeNode(std::make_unique<T>(std::forward<Args>(args)...));
+        }
+
+        template <typename T>
+        const T *makeNode(std::initializer_list<const Node *> children)
+        {
+            return storeNode(std::make_unique<T>(children));
+        }
+
+        const FunctionNode *makeFunctionNode(const Function &f, std::vector<const Node *> children)
+        {
+            return storeNode(std::make_unique<FunctionNode>(f, children));
+        }
+    };
+}
